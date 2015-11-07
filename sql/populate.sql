8bc1d07e674cabd85a7dfc6cc25ee174ea5e3b18
Walid Sultan <walidalysultan@gmail.com>
2015-11-07 15:44:31 -0600

    Updated populate.sql

--- sql/populate.sql
+++ sql/populate.sql
@@ -1,2 +1,4 @@
 INSERT INTO `mumscrum`.`sprint` (`endDate`, `name`, `startDate`) VALUES ('2015-12-14', 'Sprint 1', '2015-12-1');
 INSERT INTO `mumscrum`.`sprint` (`endDate`, `name`, `startDate`) VALUES ('2015-12-28', 'Sprint 2', '2015-12-15');
+INSERT INTO `mumscrum`.`scrumrelease` ( `name`) VALUES ('Release 1');
+INSERT INTO `mumscrum`.`scrumrelease` ( `name`) VALUES ('Release 2');
